358090b376bd3ea1cbb73a5b087ce16ebd3b45e2
Niels Buchholz <niels.buchholz.extern@innogy.com>
2018-11-26 10:35:39 +0100

    Handle error when beer is not available at supplier

--- beershop/src/main/java/de/innogy/emobility/springtraining/beershop/model/BeerItem.java
+++ beershop/src/main/java/de/innogy/emobility/springtraining/beershop/model/BeerItem.java
@@ -1,6 +1,7 @@
 package de.innogy.emobility.springtraining.beershop.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,6 +15,7 @@ import javax.validation.constraints.NotNull;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Builder
 @Table(name="BEER_ITEM")
 public class BeerItem {
 
